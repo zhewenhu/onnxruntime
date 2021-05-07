8237f239641c0e42857d9edc7831321a29e72e69
Tianlei Wu <tlwu@microsoft.com>
2021-05-07 02:21:03 -0700

    skip ms contrib ops

--- onnxruntime/python/tools/symbolic_shape_infer.py
+++ onnxruntime/python/tools/symbolic_shape_infer.py
@@ -328,7 +328,7 @@ class SymbolicShapeInference:
 
     def _onnx_infer_single_node(self, node):
         # skip onnx shape inference for some ops, as they are handled in _infer_* or it is not in onnx domain
-        skip_infer = node.op_type in ['If', 'Loop', 'Scan', 'SplitToSequence', 'ZipMap', 'LayerNormalization']
+        skip_infer = (node.domain in ['com.microsoft']) or (node.op_type in ['If', 'Loop', 'Scan', 'SplitToSequence', 'ZipMap'])
 
         if not skip_infer:
             # Some operators need initializers for shape inference. For example, Unsqueeze in opset 13 has axes in the second input.
