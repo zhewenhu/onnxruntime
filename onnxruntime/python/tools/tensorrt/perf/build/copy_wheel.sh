f15f38a98f5dee87931ad6b428d8e0f43ff3313e
Olivia Jain <oljain@microsoft.com>
2021-06-08 21:43:36 -0700

    create outer folder ort-trt-ep

--- onnxruntime/python/tools/tensorrt/perf/build/copy_wheel.sh
+++ onnxruntime/python/tools/tensorrt/perf/build/copy_wheel.sh
@@ -7,6 +7,11 @@ p) PERF_DIR=${OPTARG};;
 esac
 done 
 
+WHEEL_FOLDER=ort-trt-ep
+
+cd $PERF_DIR
+rm -rf $WHEEL_FOLDER
+mkdir $WHEEL_FOLDER
 id=$(sudo docker create ort-master)
-sudo docker cp $id:/code/onnxruntime/build/Linux/Release/dist/ $PERF_DIR 
+sudo docker cp $id:/code/onnxruntime/build/Linux/Release/dist/ $PERF_DIR/$WHEEL_FOLDER
 sudo docker rm -v $id
